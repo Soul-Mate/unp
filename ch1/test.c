a21c2af6a6a6669c78c475893c56a257f3c02482
Yuanchang <yuanchang.xu@outlook.com>
2018-01-31 21:36:05 +0800

    init

---
+++ ch1/test.c
@@ -0,0 +1,7 @@
+#include "unp.h"
+
+int main(void)
+{
+    err_quit("hello world\n");
+    return 0;
+}
---
+++ libs/unp.h
@@ -0,0 +1,21 @@
+//
+// Created by xyc on 18-1-31.
+//
+
+#ifndef PROJECT_UNP_H
+#define PROJECT_UNP_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#define MAXLINE 4096
+
+void	 err_dump(const char *, ...);
+void	 err_msg(const char *, ...);
+void	 err_quit(const char *, ...);
+void	 err_ret(const char *, ...);
+void	 err_sys(const char *, ...);
+
+#endif //PROJECT_UNP_H
